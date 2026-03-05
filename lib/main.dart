0bfa2ca6ef7812d27ff30a037381ec2a7fc22231
jpbalan-bam <juan.balan@bam.com.gt>
2026-03-04 21:50:37 -0600

    Add intl dependencie to translate app english and spanish use l10n

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_course/features/auth/login/views/login_view.dart';
+import 'package:flutter_course/l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      supportedLocales: const [Locale('es', 'ES'), Locale('en', 'US'),],
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       home: const LoginView(),
     );
   }
