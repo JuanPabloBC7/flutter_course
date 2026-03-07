6e2ad8fb21db47bf6abde5496eecc96893a5e9f2
jpbalan-bam <juan.balan@bam.com.gt>
2026-03-07 12:45:38 -0600

    Initial commit - Create a Argon Creative Tim Template

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_course/core/routing/routes.dart';
-import 'package:flutter_course/features/auth/login/views/login_view.dart';
+// import 'package:flutter_course/core/routing/routes.dart';
+// import 'package:flutter_course/features/auth/login/views/login_view.dart';
 import 'package:flutter_course/l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+
+// screens
+import 'package:flutter_course/features/views/onboarding.dart';
+import 'package:flutter_course/features/views/pro.dart';
+import 'package:flutter_course/features/views/home.dart';
+import 'package:flutter_course/features/views/profile.dart';
+import 'package:flutter_course/features/views/register.dart';
+import 'package:flutter_course/features/views/articles.dart';
+import 'package:flutter_course/features/views/elements.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,9 +30,23 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      home: const LoginView(),
-      initialRoute: AppRoutes.login,
-      onGenerateRoute: AppRoutes.generateRoute,
+      // home: const LoginView(),
+      // initialRoute: AppRoutes.login,
+      // onGenerateRoute: AppRoutes.generateRoute,
+
+      title: 'Argon PRO Flutter',
+      theme: ThemeData(fontFamily: 'OpenSans'),
+      initialRoute: "/onboarding",
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder>{
+        "/onboarding": (BuildContext context) => new Onboarding(),
+        "/home": (BuildContext context) => new Home(),
+        "/profile": (BuildContext context) => new Profile(),
+        "/articles": (BuildContext context) => new Articles(),
+        "/elements": (BuildContext context) => new Elements(),
+        "/account": (BuildContext context) => new Register(),
+        "/pro": (BuildContext context) => new Pro(),
+      }
     );
   }
 }
